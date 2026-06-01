8730f50e0671b05e3e3748a41b38b2637f930613
VitorPrates <vitoraaprates@gmail.com>
2026-06-01 19:07:03 -0300

    design atualizado para o novo modelo

--- funks.js
+++ funks.js
@@ -2,6 +2,128 @@ const btns_opcao_desafio = document.querySelectorAll(".btn_opcao_desafio")
 const telas_desafios = document.querySelectorAll(".tela_desafio")
 const background_dashboard = document.getElementById("Display_dashboard")
 
+//Elementos 3D - elementos
+const cube = document.getElementById('cube');
+let rodando = false
+
+let isDragging = false;
+let previousX = 0;
+let previousY = 0;
+
+let rotateX = -20;
+let rotateY = 30;
+//Fim elementos 3D - elementos
+
+//Elementos 3D - funções
+cube.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    document.body.style.userSelect = 'none';
+    previousX = e.clientX;
+    previousY = e.clientY;
+});
+
+window.addEventListener('mouseup', () => {
+    isDragging = false;
+    document.body.style.userSelect = '';
+    // console.log(`X: ${rotateX} / Y: ${rotateY}`);
+});
+
+window.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+
+    const deltaX = e.clientX - previousX;
+    const deltaY = e.clientY - previousY;
+
+    rotateY += deltaX * 0.15;
+    rotateX -= deltaY * 0.15;
+
+    cube.style.transform =
+        `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+    previousX = e.clientX;
+    previousY = e.clientY;
+});
+
+function rodar(pos)
+{
+    if(!rodando)
+    {
+        rodando = true
+        let alvoX = 0;
+        let alvoY = 0;
+
+        switch (pos)
+        {
+            case 1:
+                alvoX = 0;
+                alvoY = 0;
+                break;
+
+            case 2:
+                alvoX = 0;
+                alvoY = 180;
+                break;
+
+            case 3:
+                alvoX = 0;
+                alvoY = 270;
+                break;
+
+            case 4:
+                alvoX = 0;
+                alvoY = 90;
+                break;
+
+            case 5:
+                alvoX = -90;
+                alvoY = 0;
+                break;
+
+            case 6:
+                alvoX = 90;
+                alvoY = 0;
+                break;
+        }
+
+        function animar()
+        {
+            const velocidade = 0.15;
+
+            rotateX += (alvoX - rotateX) * velocidade;
+            rotateY += (alvoY - rotateY) * velocidade;
+
+            cube.style.transform =
+                `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+            const chegouX = Math.abs(alvoX - rotateX) < 0.1;
+            const chegouY = Math.abs(alvoY - rotateY) < 0.1;
+
+            if (!chegouX || !chegouY)
+            {
+                requestAnimationFrame(animar);
+            }
+            else
+            {
+                rotateX = alvoX;
+                rotateY = alvoY;
+                cube.style.transform =
+                    `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+                rodando = false
+            }
+        }
+        requestAnimationFrame(animar);
+    }
+    
+}
+
+function acoes_rotacao()
+{
+    
+}
+
+//Fim- elementos 3d - funções
+
+
 //desafio 1
 const dolar_atual_view = document.querySelector(".dolar_atual")
 const real_atual_view = document.querySelector(".real_atual")
@@ -23,11 +145,12 @@ btns_opcao_desafio.forEach((btn,index) =>{
         e.preventDefault()
         background_dashboard.style.backgroundColor = `var(--cor${e.target.innerText.at(-1)})`
         console.log(e.target.innerText.at(-1));
+        rodar(index+1)
         telas_desafios.forEach((tela,index) => {
             tela.classList.remove("selecionado")
             tela.style.borderColor = `var(--cor${index+1})`
         })
-        telas_desafios[e.target.innerText.at(-1)-1].classList.add("selecionado")
+        // telas_desafios[e.target.innerText.at(-1)-1].classList.add("selecionado")
     })
 })
 
@@ -76,6 +199,9 @@ form_imc.addEventListener("input", (e) =>{
         }
     }
 })
+
+
+
 function calcular_imc()
 {
 
